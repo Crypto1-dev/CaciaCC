243bb6eefb67f8255c7e7eebd07eb85445156d71
Zone-crypto-ZNE <empeyjj@icloud.com>
2025-04-06 15:43:07 -0500

    Update network.rs

--- src/network.rs
+++ src/network.rs
@@ -6,31 +6,33 @@ use crate::{Blockchain, Block, Transaction};
 
 #[derive(Clone)]
 pub struct Network {
-    bc: Arc<Mutex<Blockchain>>,
-    addr: String,
-    peers: Vec<String>,
+    pub bc: Arc<Mutex<Blockchain>>,
+    pub addr: String,
+    pub peers: Vec<String>,
 }
 
 impl Network {
     pub fn new(bc: Arc<Mutex<Blockchain>>, addr: String, peers: Vec<String>) -> Self {
-        Network { bc, addr, peers }
+        Self { bc, addr, peers }
     }
 
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         let listener = TcpListener::bind(&self.addr).await?;
         println!("P2P server listening on {}", self.addr);
 
+        // Connect to known peers
         for peer in &self.peers {
             self.connect_to_peer(peer).await;
         }
 
-        while let Ok((stream, addr)) = listener.accept().await {
-            let bc = Arc::clone(&self.bc);
+        // Accept incoming connections continuously
+        loop {
+            let (stream, addr) = listener.accept().await?;
+            let bc_clone = Arc::clone(&self.bc);
             tokio::spawn(async move {
-                Self::handle_connection(stream, addr, bc).await;
+                Self::handle_connection(stream, addr, bc_clone).await;
             });
         }
-        Ok(())
     }
 
     async fn connect_to_peer(&self, peer: &str) {
@@ -39,42 +41,51 @@ impl Network {
                 println!("Connected to peer {}", peer);
                 let chain = self.bc.lock().unwrap().get_chain();
                 let msg = serde_json::to_string(&chain).unwrap();
-                stream.write_all(msg.as_bytes()).await.unwrap();
+                if let Err(e) = stream.write_all(msg.as_bytes()).await {
+                    println!("Error sending chain to {}: {}", peer, e);
+                }
             }
-            Err(e) => println!("Failed to connect to {}: {}", peer, e),
+            Err(e) => println!("Failed to connect to peer {}: {}", peer, e),
         }
     }
 
     async fn handle_connection(mut stream: TcpStream, addr: std::net::SocketAddr, bc: Arc<Mutex<Blockchain>>) {
-        let mut buffer = [0; 4096];
+        let mut buffer = [0u8; 4096];
         match stream.read(&mut buffer).await {
             Ok(n) if n > 0 => {
-                let msg = String::from_utf8_lossy(&buffer[..n]);
-                println!("Received from {}: {}", addr, msg.len());
+                let msg = String::from_utf8_lossy(&buffer[..n]).to_string();
+                println!("Received {} bytes from {}", n, addr);
 
+                // Attempt to deserialize as a chain first
                 if let Ok(chain) = serde_json::from_str::<Vec<Block>>(&msg) {
-                    let mut bc = bc.lock().unwrap();
-                    if chain.len() > bc.chain.len() {
+                    let mut bc_lock = bc.lock().unwrap();
+                    if chain.len() > bc_lock.chain.len() {
                         println!("Syncing chain from peer {}", addr);
-                        bc.chain = chain.into_iter().collect();
-                    }
-                } else if let Ok(block) = serde_json::from_str::<Block>(&msg) {
-                    let mut bc = bc.lock().unwrap();
-                    if block.index == bc.chain.back().unwrap().index + 1 &&
-                       block.previous_hash == bc.chain.back().unwrap().hash {
-                        bc.apply_block(block);
-                        println!("Applied block from peer {}", addr);
-                    }
-                } else if let Ok(tx) = serde_json::from_str::<Transaction>(&msg) {
-                    let mut bc = bc.lock().unwrap();
-                    if bc.add_transaction(tx) {
-                        println!("Added tx from peer {}", addr);
+                        // Assume incoming chain is valid; ideally, run validate_chain() here
+                        bc_lock.chain = chain.into_iter().collect();
                     }
                 }
-
-                stream.write_all(b"ACK").await.unwrap();
+                // Attempt to deserialize as a block
+                else if let Ok(block) = serde_json::from_str::<Block>(&msg) {
+                    let mut bc_lock = bc.lock().unwrap();
+                    if block.index == bc_lock.chain.back().unwrap().index + 1 &&
+                       block.previous_hash == bc_lock.chain.back().unwrap().hash {
+                        bc_lock.apply_block(block);
+                        println!("Applied block from peer {}", addr);
+                    }
+                }
+                // Attempt to deserialize as a transaction
+                else if let Ok(tx) = serde_json::from_str::<Transaction>(&msg) {
+                    let mut bc_lock = bc.lock().unwrap();
+                    if bc_lock.add_transaction(tx) {
+                        println!("Added transaction from peer {}", addr);
+                    }
+                } else {
+                    println!("Unrecognized message format from peer {}", addr);
+                }
+                let _ = stream.write_all(b"ACK").await;
             }
-            Ok(_) => println!("Empty message from {}", addr),
+            Ok(_) => println!("Received empty message from {}", addr),
             Err(e) => println!("Error reading from {}: {}", addr, e),
         }
     }
@@ -83,7 +94,11 @@ impl Network {
         let msg = serde_json::to_string(&block).unwrap();
         for peer in &self.peers {
             if let Ok(mut stream) = TcpStream::connect(peer).await {
-                stream.write_all(msg.as_bytes()).await.unwrap();
+                if let Err(e) = stream.write_all(msg.as_bytes()).await {
+                    println!("Error broadcasting block to {}: {}", peer, e);
+                }
+            } else {
+                println!("Could not connect to peer {}", peer);
             }
         }
     }
@@ -92,7 +107,11 @@ impl Network {
         let msg = serde_json::to_string(&tx).unwrap();
         for peer in &self.peers {
             if let Ok(mut stream) = TcpStream::connect(peer).await {
-                stream.write_all(msg.as_bytes()).await.unwrap();
+                if let Err(e) = stream.write_all(msg.as_bytes()).await {
+                    println!("Error broadcasting transaction to {}: {}", peer, e);
+                }
+            } else {
+                println!("Could not connect to peer {}", peer);
             }
         }
     }
